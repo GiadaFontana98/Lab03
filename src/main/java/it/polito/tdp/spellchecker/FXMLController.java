b929afafcc323be8d758ace1ab3e50f9239db503
39327 <39327@172.22.173.22>
2022-03-22 14:43:54 +0100

    Quasi finito, presenta qualche errore

--- src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,25 +1,72 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.spellchecker.model.Dictionary;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class FXMLController {
+	
+	Dictionary model = new Dictionary();
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML
+    private ComboBox<String> cmbLanguage;
+
+    @FXML
+    private TextArea txtCorr;
+
+    @FXML
+    private Label txtErrori;
+
+    @FXML
+    private Label txtTempo;
+
+    @FXML
+    private TextArea txtTesto;
+
+    @FXML
+    void check(ActionEvent event) {
+      model.loadDictionary(cmbLanguage.getPromptText());  //serve per stamparmi il risultato, prende quello che c'è nella cmb
+      txtCorr.setText(""+model.spellCheckTest(txtTesto.getText()).size());
+      txtErrori.setText(""+model.spellCheckTest(txtTesto.getText()).size());
+      txtTempo.setText(""+model.getTemp());
+      
+    
+    }
+
+    @FXML
+    void clear(ActionEvent event) {
 
     }
+
+    public void setModel(Dictionary model)
+    {
+    	this.model=model;
+    }
+    
+    @FXML
+    void initialize() {
+        assert cmbLanguage != null : "fx:id=\"cmbLanguage\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtCorr != null : "fx:id=\"txtCorr\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtErrori != null : "fx:id=\"txtErrori\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTempo != null : "fx:id=\"txtTempo\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTesto != null : "fx:id=\"txtTesto\" was not injected: check your FXML file 'Scene.fxml'.";
+
+        cmbLanguage.getItems().add("English"); 
+        cmbLanguage.getItems().add("Italian"); 
+    }
+
 }
 
 
---
+++ src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -0,0 +1,77 @@
+package it.polito.tdp.spellchecker.model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class Dictionary {
+   
+	Set<String>dizionario= new HashSet<String>();
+	//utente puo scegliere due lingue, io mi conservo nella stringa tutte le parole.
+	//ogni volta leggo la parola e la conservo, faccio le letture dal file.
+	
+	//Adesso controllo l'efficienza
+	long start,end, temp=0;
+	
+	
+	public long getTemp() {
+		return temp;
+	}
+
+	public void setTemp(long temp) {
+		this.temp = temp;
+	}
+
+	public void loadDictionary(String language)
+	{
+		String l=null;
+	if(language.compareTo("English")==0)
+	{
+		l="C:\\Users\\39327\\git\\Lab03\\src\\main\\resources\\English.txt";
+	}
+		else
+		{
+			l="C:\\Users\\39327\\git\\Lab03\\src\\main\\resources\\Italian.txt";
+		}
+	
+
+		
+		try {
+			FileReader fr = new FileReader(l);
+			BufferedReader br = new BufferedReader(fr);
+			String word;
+			while ((word = br.readLine()) != null) {
+			// Aggiungere parola alla struttura dati
+			}
+			br.close();
+			} catch (IOException e){
+			System.out.println("Errore nella lettura del file");
+			}
+	}
+   
+	//qui verifico il primo passsaggio, le robe che mette l'utente
+	public List<String>spellCheckTest(String testo)
+	{ 
+		List<String>paroleErrate= new LinkedList<String>();
+		testo=testo.toLowerCase(); //mi porto tutto il testo in minuscolo, cosi non ho problemi per svolgere la traduzione
+		testo.replaceAll("[.,\\/#!$%\\^&\\*;:{}=\\-_`~()\\[\\]\"]", "");
+		String [] parole=testo.split("");
+		start=System.nanoTime();
+		for(String p : parole)
+		{
+			if(dizionario.contains(p.toLowerCase()))
+			{
+				paroleErrate.add(p);
+			}
+		}
+		end=System.nanoTime();
+		temp=end-start;
+		return paroleErrate;
+	}
+  }
+
